576bc3b1324e5334d911e8364728f191c240b878
jbrant-admin <jbrant-admin@example.com>
2023-05-02 09:33:07 -0400

    123

--- public/sw.js
+++ public/sw.js
@@ -2,10 +2,20 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
 );
 
-// This is your Service Worker, you can put any of your custom Service Worker
-// code in this file, above the `precacheAndRoute` line.
+
+self.addEventListener('push', () => {
+  event.waitUntil(
+    registration.showNotification("Hello!", {
+      body: "This is a push notification!",
+    })
+  );
+});
+
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+
+
+
 /*
 workbox.routing.registerRoute(
   ({request}) => request.destination === 'file',
